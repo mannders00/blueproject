ba2f2c7048315075e99712ce38c1ffa3630ccb7a
matta9001 <matta9001@gmail.com>
2023-03-04 17:12:15 -0600

    Add sqlite3

--- main.go
+++ main.go
@@ -1,19 +1,44 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
-type Data struct {
-	Loc   string
-	Price string
+var db sql.DB
+
+type Contractor struct {
+	ID          int
+	Name        string
+	Description string
+	Lng         float64
+	Lat         float64
 }
 
-var d Data
-
 func getIndexHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Open database
+	db, err := sql.Open("sqlite3", "data.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Create initial tabe
+	query := `
+		CREATE TABLE IF NOT EXISTS contractors
+		(id INTEGER PRIMARY KEY, name TEXT, description TEXT, lng NUMERIC, lat NUMERIC)
+	`
+	_, err = db.Exec(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// Parse the index.html template file
 	tmpl, err := template.ParseFiles("index.html")
 	if err != nil {
@@ -21,23 +46,28 @@ func getIndexHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	rows, err := db.Query("SELECT id, name, description, lng, lat FROM contractors")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var ctrs []Contractor
+	for rows.Next() {
+		var c Contractor
+		err = rows.Scan(&c.ID, &c.Name, &c.Description, &c.Lng, &c.Lat)
+		if err != nil {
+			log.Fatal(err)
+		}
+		ctrs = append(ctrs, c)
+	}
+
 	// Execute the template, passing any necessary data as a parameter
-	err = tmpl.Execute(w, d)
+	err = tmpl.Execute(w, ctrs)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-}
-
-func postIndexHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	location := r.FormValue("locationInput")
-	price := r.FormValue("priceInput")
-	d = Data{
-		Loc:   location,
-		Price: price,
-	}
-	getIndexHandler(w, r)
 }
 
 func main() {
@@ -47,8 +77,6 @@ func main() {
 		switch r.Method {
 		case "GET":
 			getIndexHandler(w, r)
-		case "POST":
-			postIndexHandler(w, r)
 		}
 	})
 
