1ac567159c4460865a8e7621e9eb9f8dd2fe9140
matta9001 <matta9001@gmail.com>
2023-03-21 14:17:59 -0500

    Switched to echo framework
    Now bear with me here
    
    I realize I just made an argument for learning the standard library, but not only is the echo framework simplify my code considerably, the authentication framework i will be using has support for echo, but not the standard library.
    
    Which is all fine, because at the end of the day the reason frameworks exist is because developer sat at their computer with the standard library and decided some things needed simplifying.
    
    Learning this lesson for myself and consciously picking a framework is a good conclusion.

--- go.mod
+++ go.mod
@@ -5,7 +5,18 @@ go 1.19
 require github.com/mattn/go-sqlite3 v1.14.16
 
 require (
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+)
+
+require (
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/labstack/echo/v4 v4.10.2
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/volatiletech/authboss v2.4.1+incompatible // indirect
 	golang.org/x/crypto v0.7.0 // indirect
--- main.go
+++ main.go
@@ -4,79 +4,59 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
 
-	// Register HTTP endpoints
-	http.HandleFunc("/board", boardHandler)
-	http.HandleFunc("/compose", composeHandler)
+	e := echo.New()
 
-	// Create access to public static files.
-	fileServer := http.FileServer(http.Dir("public"))
-	http.Handle("/public/", http.StripPrefix("/public", fileServer))
+	e.GET("/board", getBoard)
+	e.GET("/compose", getCompose)
+	e.POST("/compose", postCompose)
+	e.Static("/public", "public")
 
 	// Initialize Database
 	db := InitDB()
 	fmt.Println(db)
 
 	// Start Server
-	fmt.Println("listening on port 8080...")
-	go http.ListenAndServe(":8080", nil)
-	select {}
+	e.Logger.Fatal(e.Start(":8080"))
 
-}
-
-// HTTP /board
-func boardHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		getBoard(w, r)
-	}
 }
 
 // GET /board
-func getBoard(w http.ResponseWriter, r *http.Request) {
+func getBoard(c echo.Context) error {
 	tmpl := template.Must(template.ParseFiles("public/templates/header.tmpl", "public/html/board.html"))
-	err := tmpl.ExecuteTemplate(w, "board.html", nil)
+	err := tmpl.ExecuteTemplate(c.Response().Writer, "board.html", nil)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(c.Response().Writer, err.Error(), http.StatusInternalServerError)
 	}
-}
-
-// HTTP /compose
-func composeHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		getCompose(w, r)
-	case http.MethodPost:
-		postCompose(w, r)
-	}
+	return nil
 }
 
 // GET /compose
-func getCompose(w http.ResponseWriter, r *http.Request) {
+func getCompose(c echo.Context) error {
 	tmpl := template.Must(template.ParseFiles("public/templates/header.tmpl", "public/html/compose.html"))
-	err := tmpl.ExecuteTemplate(w, "compose.html", nil)
+	err := tmpl.ExecuteTemplate(c.Response().Writer, "compose.html", nil)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(c.Response().Writer, err.Error(), http.StatusInternalServerError)
 	}
+	return nil
 }
 
 // POST /compose
-func postCompose(w http.ResponseWriter, r *http.Request) {
+func postCompose(c echo.Context) error {
 
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
+	problem := c.FormValue("problem")
+	target := c.FormValue("target")
+	features := c.FormValue("features")
+	resources := c.FormValue("resources")
+	success := c.FormValue("success")
 
-	problem := r.FormValue("problem")
-	target := r.FormValue("target")
-	features := r.FormValue("features")
-	resources := r.FormValue("resources")
-	success := r.FormValue("success")
-
-	GenerateDetailsFromPrompt(problem, target, features, resources, success)
-	getCompose(w, r)
+	//GenerateDetailsFromPrompt(problem, target, features, resources, success)
+	fmt.Println(problem, target, features, resources, success)
+	getCompose(c)
+	return nil
 }
