19b77bac41108638aae0a855422972573ad6c42a
matta9001 <matta9001@gmail.com>
2023-03-25 23:50:03 -0500

    Actually got ory cloud auth working quite quickly.

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require github.com/mattn/go-sqlite3 v1.14.16
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"net/http"
@@ -10,43 +11,50 @@ import (
 
 	"github.com/gorilla/mux"
 	_ "github.com/mattn/go-sqlite3"
-	"golang.org/x/crypto/bcrypt"
+	ory "github.com/ory/client-go"
 )
 
 var db *sql.DB
 
+type App struct {
+	ory *ory.APIClient
+}
+
+const proxyPort = 4000
+
 func main() {
 
-	InitDB()
-	var err error
-	db, err = sql.Open("sqlite3", "./db.db")
-	if err != nil {
-		panic(err)
+	c := ory.NewConfiguration()
+	c.Servers = ory.ServerConfigurations{{URL: fmt.Sprintf("http://localhost:%d/.ory", proxyPort)}}
+	fmt.Println(c.Servers)
+	app := &App{
+		ory: ory.NewAPIClient(c),
 	}
-	defer db.Close()
 
 	router := mux.NewRouter()
+
+	router.HandleFunc("/", getIndex).Methods("GET")
 
 	router.HandleFunc("/board", getBoard).Methods("GET")
 
 	router.HandleFunc("/compose", getCompose).Methods("GET")
 	router.HandleFunc("/compose", postCompose).Methods("POST")
 
-	router.HandleFunc("/profile", getProfile).Methods("GET")
-
-	router.HandleFunc("/login", getLogin).Methods("GET")
-	router.HandleFunc("/login", postLogin).Methods("POST")
-
-	router.HandleFunc("/register", getRegister).Methods("GET")
-	router.HandleFunc("/register", postRegister).Methods("POST")
-
-	router.HandleFunc("/verify/{token}", getVerify).Methods("GET")
+	router.HandleFunc("/profile", app.sessionMiddleware(app.profileHandler())).Methods("GET")
 
 	router.PathPrefix("/public/").HandlerFunc(serveStatic)
 
 	fmt.Println("starting on http://localhost:8080")
 	http.ListenAndServe(":8080", router)
 	select {}
+}
+
+func getIndex(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("public/templates/header.tmpl", "public/html/index.html"))
+	err := tmpl.ExecuteTemplate(w, "index.html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func getBoard(w http.ResponseWriter, r *http.Request) {
@@ -77,114 +85,18 @@ func postCompose(w http.ResponseWriter, r *http.Request) {
 	getCompose(w, r)
 }
 
-func getProfile(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("public/html/profile.html"))
-	err := tmpl.ExecuteTemplate(w, "profile.html", nil)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func getLogin(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("public/templates/header.tmpl", "public/html/login.html"))
-	err := tmpl.ExecuteTemplate(w, "login.html", nil)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func postLogin(w http.ResponseWriter, r *http.Request) {
-	email := r.FormValue("email")
-	password := r.FormValue("password")
-	fmt.Println(email, password)
-
-	var hashedPassword string
-	err := db.QueryRow("SELECT password FROM users WHERE email = ?", email).Scan(&hashedPassword)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			http.Error(w, "User not found", http.StatusNotFound)
-		} else {
-			http.Error(w, "Error querying database", http.StatusInternalServerError)
+func (app *App) getProfile() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("public/html/profile.html"))
+		session, err := json.Marshal(getSession(r.Context()))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
-		return
-	}
-
-	err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
-	if err != nil {
-		http.Error(w, "Incorrect password", http.StatusInternalServerError)
-		return
-	}
-
-	// Check verified
-	fmt.Println("succeeded!")
-}
-
-func getRegister(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("public/templates/header.tmpl", "public/html/register.html"))
-	err := tmpl.ExecuteTemplate(w, "register.html", nil)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func postRegister(w http.ResponseWriter, r *http.Request) {
-	email := r.FormValue("email")
-	password := r.FormValue("password")
-
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	if err != nil {
-		http.Error(w, "Error hashing password.", http.StatusInternalServerError)
-		return
-	}
-
-	token := generateToken()
-
-	stmt, err := db.Prepare("INSERT INTO users(email, password, verification_token) VALUES(?, ?, ?)")
-	if err != nil {
-		http.Error(w, "Error preparing statement", http.StatusInternalServerError)
-		return
-	}
-
-	_, err = stmt.Exec(email, string(hashedPassword), token)
-	if err != nil {
-		http.Error(w, "Error registering user", http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Println(email, string(hashedPassword), token)
-
-	// Send verify email.
-	sendVerificationEmail(email, token)
-
-	// Check verified
-	fmt.Fprintf(w, "User %s registered successfully.", email)
-}
-
-func getVerify(w http.ResponseWriter, r *http.Request) {
-	token := mux.Vars(r)["token"]
-
-	var email string
-	err := db.QueryRow("SELECT email FROM users WHERE verification_token = ?", token).Scan(&email)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			http.Error(w, "Invalid token", http.StatusBadRequest)
-		} else {
-			http.Error(w, "Error querying database", http.StatusInternalServerError)
+		err = tmpl.ExecuteTemplate(w, "profile.html", string(session))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
-		return
 	}
-
-	stmt, err := db.Prepare("UPDATE users SET verified = 1, verification_token = '' WHERE email = ?")
-	if err != nil {
-		http.Error(w, "Error preparing statement", http.StatusInternalServerError)
-		return
-	}
-
-	_, err = stmt.Exec(email)
-	if err != nil {
-		http.Error(w, "Error updating user", http.StatusInternalServerError)
-	}
-
 }
 
 func serveStatic(w http.ResponseWriter, r *http.Request) {
